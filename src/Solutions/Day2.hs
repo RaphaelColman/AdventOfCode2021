b31f3c5d52ffddc84b3e42ec4c174716931e8359
Raphael Colman <raphael.colman@infinityworks.com>
2021-12-02 06:51:21 +0000

    day 2

--- src/Solutions/Day2.hs
+++ src/Solutions/Day2.hs
@@ -3,18 +3,76 @@ module Solutions.Day2
   ) where
 
 import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+                                      printSolutions, printTestSolutions)
+import           Data.Foldable       (Foldable (foldl'))
+import           Linear              (V2 (..))
+import           Text.Trifecta       (Parser, TokenParsing (token), integer,
+                                      letter, some, whiteSpace)
 
 aoc2 :: IO ()
 aoc2 = do
   printSolutions 2 $ MkAoCSolution parseInput part1 part2
 
-parseInput :: Parser String
-parseInput = undefined
+data Direction
+  = Forward
+  | Up
+  | Down
+  deriving (Show, Eq, Enum)
 
-part1 :: String -> String
-part1 = undefined
+data Instruction =
+  MkInstruction
+    { _direction :: Direction
+    , _amount    :: Integer
+    }
+  deriving (Show, Eq)
 
-part2 :: String -> String
-part2 = undefined
+type Position = V2 Integer
+
+data PositionWithAim =
+  MkPositionWithAim
+    { _position :: Position
+    , _aim      :: Integer
+    }
+  deriving (Show, Eq)
+
+parseInput :: Parser [Instruction]
+parseInput = some $ token parseInstruction
+
+parseInstruction :: Parser Instruction
+parseInstruction = do
+  directionString <- some letter
+  direction <- getDirection directionString
+  whiteSpace
+  MkInstruction direction <$> integer
+  where
+    getDirection ds =
+      case ds of
+        "up"      -> pure Up
+        "down"    -> pure Down
+        "forward" -> pure Forward
+        _         -> fail "Unexpected direction string"
+
+part1 :: [Instruction] -> Integer
+part1 = multipliedMagnitude . foldl' (flip addInstruction) (V2 0 0)
+
+part2 :: [Instruction] -> Integer
+part2 =
+  multipliedMagnitude .
+  _position . foldl' (flip addInstructionWithAim) (MkPositionWithAim (V2 0 0) 0)
+
+addInstruction :: Instruction -> Position -> Position
+addInstruction (MkInstruction direction amount) pos =
+  case direction of
+    Up      -> pos - V2 0 amount
+    Down    -> pos + V2 0 amount
+    Forward -> pos + V2 amount 0
+
+addInstructionWithAim :: Instruction -> PositionWithAim -> PositionWithAim
+addInstructionWithAim (MkInstruction direction amount) (MkPositionWithAim position aim) =
+  case direction of
+    Up      -> MkPositionWithAim position $ aim - amount
+    Down    -> MkPositionWithAim position $ aim + amount
+    Forward -> MkPositionWithAim (position + V2 amount (aim * amount)) aim
+
+multipliedMagnitude :: V2 Integer -> Integer
+multipliedMagnitude (V2 x y) = x * y
