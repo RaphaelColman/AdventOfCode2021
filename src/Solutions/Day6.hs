dc19019dbbc1444e8f1df55334979ac9b1001fdb
Raphael Colman <raphael.colman@infinityworks.com>
2021-12-06 10:07:26 +0000

    Day 6 cleanup

--- src/Solutions/Day6.hs
+++ src/Solutions/Day6.hs
@@ -4,43 +4,29 @@ module Solutions.Day6
 
 import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
                                       printSolutions, printTestSolutions)
+import           Common.ListUtils    (freqs)
 import           Data.List           (group)
+import qualified Data.Map            as M
 import qualified Data.Sequence       as S
 import           Text.Trifecta       (Parser, commaSep, integer)
 
 aoc6 :: IO ()
-aoc6
- = do
+aoc6 = do
   printSolutions 6 $ MkAoCSolution parseInput part1
   printSolutions 6 $ MkAoCSolution parseInput part2
 
-type Fish = [FishGeneration]
+type Age = Integer
 
-data FishGeneration =
-  MkFishGeneration
-    { _number :: Integer
-    , _age    :: Integer
-    }
-  deriving (Eq, Show)
+type FishColony = M.Map Age Integer
 
 parseInput :: Parser [Integer]
 parseInput = commaSep integer
 
-initFishGenerationList :: [Integer] -> [FishGeneration]
-initFishGenerationList xs =
-  map (\fs -> MkFishGeneration (toInteger (length fs)) (head fs)) $ group xs
-
 part1 :: [Integer] -> Integer
-part1 xs = runFish xs 80
+part1 = runFishColony 80 . freqs
 
 part2 :: [Integer] -> Integer
-part2 xs = runFish xs 256
-
-runFish :: [Integer] -> Int-> Integer
-runFish xs times =
-  let generations = initFishGenerationList xs
-      iterated = iterate stepFish generations !! times
-   in sum $ map _number iterated
+part2 = runFishColony 256 . freqs
 
 modularDecrement :: Integer -> Integer
 modularDecrement i =
@@ -49,20 +35,12 @@ modularDecrement i =
         then 6
         else aged
 
-stepGeneration :: FishGeneration -> FishGeneration
-stepGeneration (MkFishGeneration number age) =
-  let aged = age - 1
-   in if aged < 0
-        then MkFishGeneration number 6
-        else MkFishGeneration number aged
-
-stepFish :: [FishGeneration] -> [FishGeneration]
-stepFish fish =
-  map
-    (\(MkFishGeneration number age) ->
-       MkFishGeneration number (modularDecrement age)) $
-  newFish : fish
+stepFishColony :: FishColony -> FishColony
+stepFishColony fc = aged
   where
-    numZeros =
-      sum $ map _number $ filter (\(MkFishGeneration _ age) -> age == 0) fish
-    newFish = MkFishGeneration numZeros 9
+    numZeros = M.findWithDefault 0 0 fc
+    newFish = M.insert 9 numZeros fc
+    aged = M.filter (/= 0) $ M.mapKeysWith (+) modularDecrement newFish
+
+runFishColony :: Int -> FishColony -> Integer
+runFishColony times fc = M.foldr (+) 0 $ iterate stepFishColony fc !! times
