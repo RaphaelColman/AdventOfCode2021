465dbd2e9dffc817a2872b32925a6d0e6685506b
Raphael Colman <raphael.colman@infinityworks.com>
2021-12-14 13:11:12 +0000

    Day 14 clean up

--- src/Solutions/Day14.hs
+++ src/Solutions/Day14.hs
@@ -6,19 +6,12 @@ module Solutions.Day14
 
 import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
                                       printSolutions, printTestSolutions)
-import           Common.ListUtils    (freqs, leastCommon, mostCommon, window2)
-import           Common.MapUtils     (mapIf)
-import           Control.Arrow       (Arrow (first))
-import           Data.Foldable       (maximumBy, minimumBy)
-import           Data.Function       (on)
+import           Common.ListUtils    (freqs, window2)
 import           Data.List           (sort)
-import           Data.Map            ((!?))
 import qualified Data.Map            as M
 import           Text.Trifecta       (CharParsing (string), Parser,
                                       TokenParsing (token), count, letter, some,
                                       whiteSpace)
-import Debug.Trace
-import Common.Debugging (traceShowIf)
 
 aoc14 :: IO ()
 aoc14 = do
@@ -27,7 +20,7 @@ aoc14 = do
 
 type Element = String
 
-type Rule = (Element, Char)
+type Rule = (String, Char)
 
 type Pair = (Char, Char)
 
@@ -46,27 +39,21 @@ parseInput = do
       pure ((a, b), insert)
   pure (element, M.fromList rules)
 
---part1 :: Template -> Int
-part1 template = mTimes - lTimes
+part1 :: Template -> Int
+part1 template = last sorted - head sorted
   where
-    run = runTemplate 10 template
+    run = runTemplateNaive 10 template
     fr = freqs run
-    (most, mTimes) = maximumBy (compare `on` snd) $ M.toList fr
-    (least, lTimes) = minimumBy (compare `on` snd) $ M.toList fr
+    sorted = sort $ M.elems $ freqs run
 
---part2 :: Template -> [(Char, Char)]
-part2 template = mTimes - lTimes
+part2 :: Template -> Int
+part2 template = last sorted - head sorted
   where
-    rt@(MkRT pairCount elementCount) = runTemplate2 40 template
-    (most, mTimes) = maximumBy (compare `on` snd) $ M.toList elementCount
-    (least, lTimes) = minimumBy (compare `on` snd) $ M.toList elementCount
+    rt@(MkRT pairCount elementCount) = runTemplate 40 template
+    sorted = sort $ M.elems elementCount
 
-
---Different for real input after step 1. It should have 5 'O's but only has 4
--- It think's there's 1 NB but there are 2...
-
-runTemplate :: Int -> Template -> String
-runTemplate times (start, rules) = iterate step start !! times
+runTemplateNaive :: Int -> Template -> String
+runTemplateNaive times (start, rules) = iterate step start !! times
   where
     step current = head current : concatMap insert (window2 current)
     insert p@(a, b) = [rules M.! p, b]
@@ -87,22 +74,18 @@ initRT (start, rules) = MkRT pc $ freqs start
   where
     pc = freqs $ window2 start
 
-runTemplate2 :: Int -> Template -> RunningTotal
-runTemplate2 times template@(_, rules) =
+runTemplate :: Int -> Template -> RunningTotal
+runTemplate times template@(_, rules) =
   iterate stepRT (initRT template) !! times
   where
     stepRT :: RunningTotal -> RunningTotal
-    stepRT rt@(MkRT pairCount elementCount) = M.foldrWithKey go rt pairCount
+    stepRT rt@(MkRT pairCount _) = M.foldrWithKey go rt pairCount
       where
         go :: Pair -> Int -> RunningTotal -> RunningTotal
-        go pair@(a, b) num rt@(MkRT pc ec) =
+        go pair@(a, b) num (MkRT pc ec) =
           let insert = rules M.! pair
               newPairs = M.fromList $ map (, num) [(a, insert), (insert, b)]
               newElementCount = M.insertWith (+) insert num ec
               withPairRemoved = M.adjust (\p -> p - num) pair pc
               newPc = M.filter (>= 0) $ M.unionWith (+) newPairs withPairRemoved
            in MkRT newPc newElementCount
-
-type Counter = M.Map Pair Int
-
-type Rules = M.Map Pair [Pair]
