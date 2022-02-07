dfff203437aadc85cbb05056bcd457352276cf98
Raphael Colman <raphael.colman@infinityworks.com>
2022-02-07 12:35:21 +0000

    Day 25

--- src/Solutions/Day25.hs
+++ src/Solutions/Day25.hs
@@ -3,19 +3,132 @@ module Solutions.Day25
   ) where
 
 import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+                                      printSolutions, printTestSolutions)
+import           Common.Debugging    (traceVectorMap)
+import           Common.Geometry     (Grid, Point,
+                                      enumerateMultilineStringToVectorMap,
+                                      renderVectorMap)
+import           Control.Lens        ((^.))
+import           Data.Function       (on, (&))
+import           Data.List           (maximumBy)
+import qualified Data.Map            as M
+import           Data.Maybe          (mapMaybe)
+import qualified Data.Set            as S
+import           Linear              (V2 (V2))
+import           Text.Trifecta       (CharParsing (anyChar), Parser, some)
 
 aoc25 :: IO ()
 aoc25 = do
-  printSolutions 25 $ MkAoCSolution parseInput part1
-  printSolutions 25 $ MkAoCSolution parseInput part2
+  printTestSolutions 25 $ MkAoCSolution parseInput part1
 
-parseInput :: Parser String
-parseInput = undefined
+data SeaCucumber
+  = East
+  | South
+  deriving (Eq, Show, Ord, Enum)
 
-part1 :: String -> String
-part1 = undefined
+data Seabed =
+  MkSeabed
+    { _cucumbers  :: Grid SeaCucumber
+    , _dimensions :: V2 Int
+    }
+  deriving (Eq, Show, Ord)
 
-part2 :: String -> String
-part2 = undefined
+parseInput :: Parser Seabed
+parseInput = do
+  allChars <- some anyChar
+  let grid =
+        M.mapMaybe toSeaCucumber $ enumerateMultilineStringToVectorMap allChars
+  let pointsAsList = M.toList grid
+  let mx = maximum $ map (\(V2 x y, _) -> x) pointsAsList
+  let my = maximum $ map (\(V2 x y, _) -> y) pointsAsList
+  pure $ MkSeabed grid $ V2 mx my
+  where
+    toSeaCucumber ch =
+      case ch of
+        '>' -> Just East
+        'v' -> Just South
+        '.' -> Nothing
+        _   -> fail "Unrecognised character"
+    compareX (V2 x1 _) (V2 x2 _) = compare x1 x2
+    compareY (V2 _ y1) (V2 _ y2) = compare y1 y2
+
+part1 sb = done
+  where
+    stepped = iterate step sb !! 58
+    done = runSeabed sb
+
+runSeabed :: Seabed -> Int
+runSeabed = go 1
+  where
+    go index sb
+      | stepped == sb = index
+      | otherwise = go (index + 1) stepped
+      where
+        stepped = step sb
+
+step :: Seabed -> Seabed
+step = stepSouth . stepEast
+
+stepEast :: Seabed -> Seabed
+stepEast (MkSeabed grid dimens@(V2 maxX maxY)) =
+  MkSeabed (M.union moved rest) dimens
+  where
+    (movable, rest) =
+      M.partitionWithKey
+        (\k v -> (v == East) && M.notMember (getNextEast k) grid)
+        grid
+    moved = M.mapKeys getNextEast movable
+    getNextEast (V2 x y) =
+      let next = x + 1
+       in if next > maxX
+            then V2 0 y
+            else V2 next y
+
+stepCucumber :: SeaCucumber -> Seabed -> Seabed
+stepCucumber sc (MkSeabed grid dimens@(V2 maxX maxY)) =
+  MkSeabed (M.union moved rest) dimens
+  where
+    (movable, rest) =
+      M.partitionWithKey
+        (\k v -> (v == South) && M.notMember (next k) grid)
+        grid
+    moved = M.mapKeys next movable
+    next pt = getNext sc pt dimens
+
+stepSouth :: Seabed -> Seabed
+stepSouth (MkSeabed grid dimens@(V2 maxX maxY)) =
+  MkSeabed (M.union moved rest) dimens
+  where
+    (movable, rest) =
+      M.partitionWithKey
+        (\k v -> (v == South) && M.notMember (getNextSouth k) grid)
+        grid
+    moved = M.mapKeys getNextSouth movable
+    getNextSouth (V2 x y) =
+      let next = y + 1
+       in if next > maxY
+            then V2 x 0
+            else V2 x next
+
+getNext :: SeaCucumber -> Point -> Point -> Point
+getNext East (V2 x y) (V2 maxX maxY) =
+  let next = x + 1
+   in if next > maxX
+        then V2 0 y
+        else V2 next y
+getNext South (V2 x y) (V2 maxX maxY) =
+  let next = y + 1
+   in if next > maxY
+        then V2 x 0
+        else V2 x next
+
+renderSeabed :: Seabed -> M.Map Point Char
+renderSeabed (MkSeabed grid _) = M.map toChar grid
+  where
+    toChar sc =
+      case sc of
+        East  -> '>'
+        South -> 'v'
+
+showSeabed :: Seabed -> IO ()
+showSeabed = putStrLn . renderVectorMap . renderSeabed
